d6e26b3da7f8c83ad9f841e780cdfa8d562ca593
Emma King <emmaking@Emmas-MacBook-Air.local>
2023-10-23 21:47:58 +0100

    Adding tables to population panel server

--- states_test/app.R
+++ states_test/app.R
@@ -1,36 +1,60 @@
 
 library(shiny)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  
   tabsetPanel(
     tabPanel("Population of US states",
       fluidRow(
         column(8,
           sliderInput(inputId = "n", label = "select number of states",
                            min = 1, max = 30, value = 1, step = 1)
-              )),
+        )
+      ),
       fluidRow(
         column(6,
-          tableOutput(outputId = "list"),
-              )),
+          tableOutput(outputId = "listpop"),
+        ),
+        column(6,
+          plotOutput(outputId = "plotpop")
+        )
+      )
+    ),
+    tabPanel("Area of US states",
+      fluidRow(
+        column(8,
+          sliderInput(inputId = "n", label = "select number of states",
+                      min = 1, max = 30, value = 1, step = 1)
+        )
+      ),
+      fluidRow(
+        column(6,
+          tableOutput(outputId = "listarea"),
+        ),
+        column(6,
+          plotOutput(outputId = "plotarea")
+        )
+      )
+    )
   )
 )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    df_population <- reactive({
+      df_state %>%
+        dplyr::arrange(desc(Population)) %>%
+        dplyr::slice(1:input$n)
     })
+    
+    output$listpop <- renderTable({
+      df_population()
+    })
+    
 }
 
 # Run the application 
